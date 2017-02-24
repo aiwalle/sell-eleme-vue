abe251b33738249e9bd6c887ba0a86a8d8dfce52
walle <wallelj0909@gmail.com>
2017-02-24 17:31:53 +0800

    update

---
+++ src/components/star/star.vue
@@ -0,0 +1,49 @@
+<template lang="html">
+  <div class="star" :class="starType">
+    <span v-for="itemClass in itemClasses" :class="itemClass" class="star-item"></span>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {
+    props: {
+      size: {
+        type: Number
+      },
+      score: {
+        type: Number
+      }
+    }
+    computed: {
+      starType() {
+        return 'star-' + this.size;
+      }
+    }
+  }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+  @import "../../common/stylus/mixin";
+
+  .star
+    font-size: 0
+    .star-item
+      display: inline-block
+      background-repeat: no-repeat
+    &.star-48
+      .star-item
+        width: 20px
+        height: 20px
+        margin-right: 22px
+        background-size: 20px 20px
+        &.on
+          bg-image('star48_on')
+        &.half
+          bg-image('star48_half')
+        &.off
+          bg-image('star48_off')
+    &.star-36
+    &.star-24
+
+
+</style>
